3d8ef2e1810bb12ece17cb4bdba9f4aaac5c7ee9
n19l <jeanpaulngalula07@gmail.com>
2020-05-14 12:38:48 +0200

    wip bio

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="home-page">
-hello home page
+	<h1>Hello.</h1>
+
+	<p class="name">My name is NGALULA Jean-Paul.</p>
+
+	<p>I am student and software developer at <a href="https://ownest.io/" target="_blank">Ownest</a>. Most of my work is publicly available on <a href="https://github.com/n19l" target="_blank">GitHub</a>.</p>
+
+	<p>You can also find me on <a href="https://www.linkedin.com/in/jeanpaulngalula/" target="_blank">LinkedIn</a> and <a href="https://twitter.com/casison7" target="_blank">Twitter</a>.</p>
+
+	<p>Outside of programming, I enjoy video games, karaoke, football and eating pondu.</p>
 </div>
 </template>
 
