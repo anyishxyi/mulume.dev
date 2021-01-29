43eaa7922545e6de2fbb3afcee3582a01c480276
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-01-29 03:45:40 +0100

    fix responsive for iphoneX

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -26,7 +26,7 @@
         <svg v-else class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
       </button>
       <!-- <router-link :to="{path: '/home'}"><a class="link color-theme-accent hidden inline-block"> start </a></router-link> -->
-      <router-link :to="{path: '/contact'}"><button class="slide hidden">Get in touch</button></router-link>
+      <router-link :to="{path: '/contact'}"><button class="slide hidden btn-pad">Get in touch</button></router-link>
     </div>
   </div>
 </template>
