ba789a51fb8dde7bfcb5f8613db1fb9b6acaf33a
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-02-02 23:25:29 +0100

    fix padding for theme-switch-btn

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,6 +11,22 @@
       <span class="pl-5 font-bold name-size color-theme-accent">Jean-Paul NGALULA</span>
     </div>
   </router-link>
+
+  <button class="theme-switch-button mb-15" @click="toggleTheme">
+    <svg v-if="!!theme" class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <circle cx="12" cy="12" r="5"></circle>
+      <line x1="12" y1="1" x2="12" y2="3"></line>
+      <line x1="12" y1="21" x2="12" y2="23"></line>
+      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+      <line x1="1" y1="12" x2="3" y2="12"></line>
+      <line x1="21" y1="12" x2="23" y2="12"></line>
+      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+    </svg>
+    <svg v-else class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+  </button>
+
   <div class="nav-btn">
     <label for="nav-check">
       <span></span>
@@ -18,22 +34,7 @@
       <span></span>
     </label>
   </div>
-  
   <div class="nav-links">
-    <button class="theme-switch-button mb-15" @click="toggleTheme">
-      <svg v-if="!!theme" class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <circle cx="12" cy="12" r="5"></circle>
-        <line x1="12" y1="1" x2="12" y2="3"></line>
-        <line x1="12" y1="21" x2="12" y2="23"></line>
-        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-        <line x1="1" y1="12" x2="3" y2="12"></line>
-        <line x1="21" y1="12" x2="23" y2="12"></line>
-        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-      </svg>
-      <svg v-else class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-    </button>
     <router-link :to="{path: '/home'}"><a class="link color-theme-accent hidden inline-block"> start </a></router-link>
     <router-link :to="{path: '/about'}"><a class="link color-theme-accent hidden inline-block"> about </a></router-link>
     <router-link :to="{path: '/contact'}"><button class="slide hidden btn-pad">Get in touch</button></router-link>
