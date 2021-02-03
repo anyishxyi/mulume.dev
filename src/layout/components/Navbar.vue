6149281071ec0ce9529b1a9cca6697c7616c9ad5
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-02-03 16:42:02 +0100

    wip about page

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -36,7 +36,7 @@
     </div>
     <div class="nav-links">
       <router-link :to="{path: '/home'}"><button for="nav-check" class="close ml-20 btn-no-border btn-pad"> start </button></router-link>
-      <!-- <router-link :to="{path: '/about'}"><button for="nav-check" class="close ml-20 btn-no-border btn-pad"> about </button></router-link> -->
+      <router-link :to="{path: '/about'}"><button for="nav-check" class="close ml-20 btn-no-border btn-pad"> about </button></router-link>
       <!-- <router-link :to="{path: '/uses'}"><button for="nav-check" class="close ml-20 btn-no-border btn-pad"> uses </button></router-link> -->
       <router-link :to="{path: '/contact'}"><button for="nav-check" class="slide ml-20 btn-border btn-pad">Get in touch</button></router-link>
     </div>
--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<div>resume</div>
+	<div class="about_page m-0 w-content">
+		<div class="heading"></div>
+		<div class="content"></div>
+	</div>
 </template>
 
 <script>
