7f70d7b49e813f3c3d9e054bff5d01f1a763ba5c
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-16 12:31:12 +0100

    fix css of svg and hide hamburger

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,8 +11,8 @@
       </div>
     </router-link>
     <div class="right-menu mr-40 justify-center vertical-middle flex align-items">
-      <button class="theme-switch-button mb-15" @click="toggleTheme">
-        <svg v-if="!!theme" class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <button class="theme-switch-button" @click="toggleTheme">
+        <svg v-if="!!theme" class="svg-theme-size mb-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <circle cx="12" cy="12" r="5"></circle>
           <line x1="12" y1="1" x2="12" y2="3"></line>
           <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -23,7 +23,7 @@
           <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
           <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
         </svg>
-        <svg v-else class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+        <svg v-else class="svg-theme-size mb-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
       </button>
       <!-- <router-link :to="{path: '/home'}"><a class="link color-theme-accent hidden inline-block"> start </a></router-link> -->
       <router-link :to="{path: '/contact'}"><button class="slide hidden btn-pad">Get in touch</button></router-link>
