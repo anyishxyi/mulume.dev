5342512d1e2b0ad81b7250f163dcc9458b37e425
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-17 18:23:25 +0100

    adding skill section

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -13,7 +13,7 @@
 			</div>
 		</div>
 		<div class="content">
-			<div class="title">Work Experience</div>
+			<div class="title">Work Experience <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="laptop-code" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-laptop-code fa-w-20"><path fill="currentColor" d="M624 352h-48V64c0-35.2-28.8-64-64-64H128C92.8 0 64 28.8 64 64v288H16c-8.8 0-16 7.2-16 16v48c0 52.8 43.2 96 96 96h448c52.8 0 96-43.2 96-96v-48c0-8.8-7.2-16-16-16zM112 64c0-8.67 7.33-16 16-16h384c8.67 0 16 7.33 16 16v288H112V64zm480 352c0 26.47-21.53 48-48 48H96c-26.47 0-48-21.53-48-48v-16h180.9c5.57 9.39 15.38 16 27.1 16h128c11.72 0 21.52-6.61 27.1-16H592v16zM277.66 261.65l11.31-11.31c6.25-6.25 6.25-16.38 0-22.63L253.25 192l35.71-35.72c6.25-6.25 6.25-16.38 0-22.63l-11.31-11.31c-6.25-6.25-16.38-6.25-22.63 0l-58.34 58.34c-6.25 6.25-6.25 16.38 0 22.63l58.34 58.34c6.25 6.25 16.39 6.25 22.64 0zm73.38-11.3l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0l58.34-58.34c6.25-6.25 6.25-16.38 0-22.63l-58.34-58.34c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63L386.75 192l-35.71 35.72c-6.25 6.25-6.25 16.38 0 22.63z" class=""></path></svg></div>
 			<div class="works">
 				<div class="work flex">
 					<div class="container">
@@ -21,8 +21,8 @@
 						<div class="ping anime-ping"></div>
 					</div>
 					<div class="content">
-						<div class="periode">Sept 2018 - AUJOURD’HUI</div>
-						<div class="role">Développeur Fullstack Javascript</div>
+						<div class="periode">September 2018 - current · France 🇫🇷</div>
+						<div class="role">Full-stack Web Developer Javascript</div>
 						<div class="entreprise">Ownest</div>
 					</div>
 				</div>
@@ -31,7 +31,7 @@
 						<div class="ping"></div>
 					</div>
 					<div class="content">
-						<div class="periode">Juin 2018 - Aout 2018</div>
+						<div class="periode">Juin 2018 - Aout 2018 · France 🇫🇷</div>
 						<div class="role">Internship | Web Developer PHP</div>
 						<div class="entreprise">Jobnroll</div>
 					</div>
@@ -41,14 +41,16 @@
 						<div class="ping"></div>
 					</div>
 					<div class="content">
-						<div class="periode">Juin 2016 - Aout 2017</div>
+						<div class="periode">Juin 2016 - Aout 2017  · Congo R.D. 🇨🇩</div>
 						<div class="role">Développeur Java/JavaEE</div>
 						<div class="entreprise">FPI</div>
 					</div>
 				</div>
 			</div>
-			<div class="title">Tech skills</div>
+			<hr>
+			<div class="title">Tech skills <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="wrench" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-wrench fa-w-16"><path fill="currentColor" d="M507.48 117.18c-3-12.17-12.41-21.79-24.5-25.15-12.1-3.34-25.16.11-33.97 8.97l-58.66 58.63-32.44-5.4-5.38-32.41 58.67-58.64c8.84-8.89 12.28-21.92 8.91-33.99-3.38-12.11-13.06-21.5-25.29-24.53-53.09-13.19-107.91 2.07-146.54 40.69-37.63 37.62-52.6 91.37-40.72 143.27L24.04 372.06C8.53 387.53 0 408.12 0 430.02s8.53 42.49 24.04 57.97C39.51 503.47 60.1 512 82.01 512c21.88 0 42.47-8.53 57.98-24.01l183.34-183.26c51.79 11.87 105.64-3.14 143.49-40.93 38.09-38.1 53.69-94.27 40.66-146.62zm-74.61 112.69c-28.47 28.46-70.2 38.1-109.01 25.21l-14.06-4.69-203.75 203.67c-12.85 12.84-35.29 12.81-48.07 0-6.44-6.42-9.97-14.96-9.97-24.04 0-9.08 3.53-17.61 9.97-24.03l203.84-203.78-4.63-14.03c-12.81-38.9-3.22-80.62 25.04-108.9 20.35-20.32 47.19-31.24 75.04-31.24h1.12l-57.32 57.3 15.13 90.59 90.57 15.09 57.35-57.29c.32 28.26-10.62 55.52-31.25 76.14zM88.01 408.02c-8.84 0-16 7.16-16 16s7.16 16 16 16 16-7.16 16-16-7.16-16-16-16z" class=""></path></svg></div>
 			<div class="skills"></div>
+			<hr>
 			<div class="title">Education</div>
 			<div class="education">
 				<div class="pulse"></div>
