8f69f20dff227812ff23ebc258779464400d9d99
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-17 20:11:29 +0100

    add images for skill

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -3,7 +3,7 @@
 		<div class="heading flex">
       <div class="profile-width">
         <figure>
-          <img :src="jeanpaul" class="profile-about-pic" alt="Profile picture of Jean-Paul NGALULA (very handsome)" />
+          <el-image :src=jeanpaul class="profile-about-pic" alt="Profile picture of Jean-Paul NGALULA (very handsome)" lazy />
         </figure>
       </div>
       <div class="bio">
@@ -49,9 +49,59 @@
 			</div>
 			<hr>
 			<div class="title">Tech skills <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="wrench" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-wrench fa-w-16"><path fill="currentColor" d="M507.48 117.18c-3-12.17-12.41-21.79-24.5-25.15-12.1-3.34-25.16.11-33.97 8.97l-58.66 58.63-32.44-5.4-5.38-32.41 58.67-58.64c8.84-8.89 12.28-21.92 8.91-33.99-3.38-12.11-13.06-21.5-25.29-24.53-53.09-13.19-107.91 2.07-146.54 40.69-37.63 37.62-52.6 91.37-40.72 143.27L24.04 372.06C8.53 387.53 0 408.12 0 430.02s8.53 42.49 24.04 57.97C39.51 503.47 60.1 512 82.01 512c21.88 0 42.47-8.53 57.98-24.01l183.34-183.26c51.79 11.87 105.64-3.14 143.49-40.93 38.09-38.1 53.69-94.27 40.66-146.62zm-74.61 112.69c-28.47 28.46-70.2 38.1-109.01 25.21l-14.06-4.69-203.75 203.67c-12.85 12.84-35.29 12.81-48.07 0-6.44-6.42-9.97-14.96-9.97-24.04 0-9.08 3.53-17.61 9.97-24.03l203.84-203.78-4.63-14.03c-12.81-38.9-3.22-80.62 25.04-108.9 20.35-20.32 47.19-31.24 75.04-31.24h1.12l-57.32 57.3 15.13 90.59 90.57 15.09 57.35-57.29c.32 28.26-10.62 55.52-31.25 76.14zM88.01 408.02c-8.84 0-16 7.16-16 16s7.16 16 16 16 16-7.16 16-16-7.16-16-16-16z" class=""></path></svg></div>
-			<div class="skills">
-				
-			</div>
+			<ul class="skills">
+				<li>
+					<el-image :src=angular lazy />
+				</li>
+				<li>
+					<el-image :src=docker lazy />
+				</li>
+				<li>
+					<el-image :src=expressjs lazy />
+				</li>
+				<li>
+					<el-image :src=firebase lazy />
+				</li>
+				<li>
+					<el-image :src=java lazy />
+				</li>
+				<li>
+					<el-image :src=javascript lazy />
+				</li>
+				<li>
+					<el-image :src=jest lazy />
+				</li>
+				<li>
+					<el-image :src=junit lazy />
+				</li>
+				<li>
+					<el-image :src=jwt lazy />
+				</li>
+				<li>
+					<el-image :src=karma lazy />
+				</li>
+				<li>
+					<el-image :src=mocha lazy />
+				</li>
+				<li>
+					<el-image :src=mongodb lazy />
+				</li>
+				<li>
+					<el-image :src=mysql lazy />
+				</li>
+				<li>
+					<el-image :src=nodejs lazy />
+				</li>
+				<li>
+					<el-image :src=spring lazy />
+				</li>
+				<li>
+					<el-image :src=sql lazy />
+				</li>
+				<li>
+					<el-image :src=vuejs lazy />
+				</li>
+			</ul>
 			<hr>
 			<div class="title">Education</div>
 			<div class="education">
@@ -106,11 +156,46 @@
 
 <script>
 const jeanpaul = require('@/assets/images/png/jeanpaul_ngalula.png')
+const angular = require('@/assets/images/skills/angular.png')
+const docker = require('@/assets/images/skills/docker.png')
+const expressjs = require('@/assets/images/skills/expressjs.png')
+const firebase = require('@/assets/images/skills/firebase.png')
+const java = require('@/assets/images/skills/java.png')
+const javascript = require('@/assets/images/skills/javascript.png')
+const jest = require('@/assets/images/skills/jest.png')
+const junit = require('@/assets/images/skills/junit.png')
+const jwt = require('@/assets/images/skills/jwt.png')
+const karma = require('@/assets/images/skills/karma.png')
+const mocha = require('@/assets/images/skills/mocha.jpeg')
+const mongodb = require('@/assets/images/skills/mongodb.jpg')
+const mysql = require('@/assets/images/skills/mysql.jpeg')
+const nodejs = require('@/assets/images/skills/nodejs.jpeg')
+const spring = require('@/assets/images/skills/spring.png')
+const sql = require('@/assets/images/skills/sql.png')
+const vuejs = require('@/assets/images/skills/vuejs.jpeg')
+
 
 export default {
   data() {
     return {
-      jeanpaul
+      jeanpaul,
+      angular,
+      docker,
+      expressjs,
+      firebase,
+      java,
+      javascript,
+      jest,
+      junit,
+      jwt,
+      karma,
+      mocha,
+      mongodb,
+      mysql,
+      nodejs,
+      spring,
+      sql,
+      vuejs
     }
   },
 }
