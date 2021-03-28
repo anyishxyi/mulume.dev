2da1a2c3d6cce22dc07906719533e31b478d8fbf
Jean-Paul NM <jeanpaulngalula07@gmail.com>
2021-03-29 00:32:23 +0200

    Fix nav (#16)
    * rm extrat marging
    
    * fix navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -42,37 +42,6 @@
     </div>
   </div>
 </div>
-  <!-- <div class="navbar space_between flex mr-10">
-    <router-link :to="{path: '/home'}" class="txt-deco-none">
-      <div class="text-lg mt-20 justify-center align-items mr-24 flex">
-        <span class="ml-20 icon-width icon-height">
-          <figure class="pic-border">
-            <img :src="jeanpaul" class="profile-pic" alt="Profile picture of Jean-Paul NGALULA (very handsome)" />
-          </figure>
-        </span>
-        <span class="pl-5 font-bold name-size color-theme-accent">Jean-Paul NGALULA</span>
-      </div>
-    </router-link>
-    <div class="right-menu mr-40 justify-center vertical-middle flex align-items">
-      <button class="theme-switch-button" @click="toggleTheme">
-        <svg v-if="!!theme" class="svg-theme-size mb-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <circle cx="12" cy="12" r="5"></circle>
-          <line x1="12" y1="1" x2="12" y2="3"></line>
-          <line x1="12" y1="21" x2="12" y2="23"></line>
-          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-          <line x1="1" y1="12" x2="3" y2="12"></line>
-          <line x1="21" y1="12" x2="23" y2="12"></line>
-          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-        </svg>
-        <svg v-else class="svg-theme-size mb-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-      </button>
-      <router-link :to="{path: '/home'}"><a class="link color-theme-accent hidden inline-block"> start </a></router-link>
-      <router-link :to="{path: '/about'}"><a class="link color-theme-accent hidden inline-block"> about </a></router-link>
-      <router-link :to="{path: '/contact'}"><button class="slide hidden btn-pad">Get in touch</button></router-link>
-    </div>
-  </div> -->
 </template>
 
 <script>
