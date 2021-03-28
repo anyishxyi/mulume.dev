115f9c51a2eac31ebd3e0a8ca6d07c1d2992bf48
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-29 01:05:44 +0200

    hide switch menu

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
     </div>
   </router-link>
   <div class="right-menu flex p13">
-    <button class="theme-switch-button mb-15" @click="toggleTheme">
+    <!-- <button class="theme-switch-button mb-15" @click="toggleTheme">
       <svg v-if="!!theme" class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <circle cx="12" cy="12" r="5"></circle>
         <line x1="12" y1="1" x2="12" y2="3"></line>
@@ -25,7 +25,7 @@
         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
       </svg>
       <svg v-else class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-    </button>
+    </button> -->
 
     <div class="nav-btn">
       <label for="nav-check">
