6b85ac9f552780818645670f2dd3a2388841d038
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-29 00:24:48 +0200

    fix navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
     </div>
   </router-link>
   <div class="right-menu flex p13">
-    <button class="theme-switch-button" @click="toggleTheme">
+    <button class="theme-switch-button mb-15" @click="toggleTheme">
       <svg v-if="!!theme" class="svg-theme-size mt-svg-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <circle cx="12" cy="12" r="5"></circle>
         <line x1="12" y1="1" x2="12" y2="3"></line>
