0766711c8f6a09e4c265d5ce24b057604abf00d4
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-05-04 03:25:23 +0200

    unused packages

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import ElementUI from 'element-ui'
 // import locale from 'element-ui/lib/locale/lang/fr'
-import Buefy from 'buefy'
-import LocalForage from 'localforage'
 
 import store from './store'
 import App from './App.vue'
@@ -12,18 +10,13 @@ import router from './router'
 import '@/assets/styles/index.scss' // global css
 import '@/assets/images/index' //for svg files
 
-import './icons'
-
-LocalForage.config({ driver: LocalForage.IndexedDB, name: 'Hand Me', version: 1.0, storeName: 'HandMe' })
-
 Vue.config.productionTip = false
 
 Vue.prototype.handleEvent = new Vue()
-Vue.prototype.$localforage = LocalForage
 
 Vue.use(ElementUI)
 
-Vue.use(Buefy, VueMeta)
+Vue.use(VueMeta)
 
 new Vue({
   render: h => h(App),
