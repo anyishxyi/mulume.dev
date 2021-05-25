5f3f8fb0fd35088f0d239133dbbfcf9e8c4e9afd
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-05-25 03:38:43 +0200

    wip display svg

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
-console.log({req})
-const requireAll = requireContext => requireContext.keys().map(requireContext)
+const requireAll = requireContext => {
+	requireContext.keys().map(requireContext)
+}
 requireAll(req)
