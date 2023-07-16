aafc39324458d32cafdc2cee2f48d529ca29d896
NGAULA, Jean-Paul <jeanpaulngalula07@gmail.com>
2023-07-16 02:00:03 +0200

    wip header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,21 +6,58 @@ import { Component, EventEmitter, Output } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   template: `
-   <nav class="navbar">
-      <div class="navbar-left">
-        <a href=""><i class="fa fa-home"></i> JP </a>
-      </div>
-      <div class="navbar-menu">
-        <ul>
-          <li><a href="">À propos</a></li>
-          <li><a href="">Projets</a></li>
-          <li><a href="">Contact</a></li>
+    <header class="c-dRmGAT">
+      <a href="/" class="c-gfsRMa c-eEqOQi">Ng</a>
+      <nav class="c-cGucJb">
+        <ul class="c-hRSBvv">
+          <li>
+            <a href="/about" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="7">About</span>
+            </a>
+          </li>
+          <li>
+            <a href="/articles" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="8">Articles</span>
+            </a>
+          </li>
+          <li>
+            <a href="/projects" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="9">Projects</span>
+            </a>
+          </li>
+          <li>
+            <a href="/talks" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="10">Talks</span>
+            </a>
+          </li>
+          <li>
+            <a href="/podcasts" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="11">Podcasts</span>
+            </a>
+          </li>
+          <li>
+            <a href="/investing" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="12">Investing</span>
+            </a>
+          </li>
+          <li>
+            <a href="/uses" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="13">Uses</span>
+            </a>
+          </li>
+          <li>
+            <a href="/reminder" class="c-gKCiCk">
+              <span class="c-cohhyn" data-projection-id="14">Reminder</span>
+            </a>
+          </li>
         </ul>
+      </nav>
+      <div class="c-cXvxHo">
+        <button type="button" aria-label="Command" class="c-gfsRMa c-gfsRMa-ieTYVcq-css">
+          <i class="c-fhGAOp ri-command-line"></i>
+        </button>
       </div>
-      <div class="navbar-right">
-        <a href="#"><i class="fa fa-user"></i> R </a>
-      </div>
-    </nav>
+    </header>
   `,
   styleUrls: ['./header.component.scss']
 })
