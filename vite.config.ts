f6a685934568fdef4a1ebcb9db0820212cd8aeab
Jean-Paul NGALULA <jeanpaulngalula07@gmail.com>
2024-04-28 15:31:31 +0200

    add a preset manually

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     analog({
+      nitro: {
+        preset: 'vercel',
+      },
       prerender: {
         routes: ['/home'],
       },
