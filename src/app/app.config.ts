944cdd1132e196e88de3674852880755c37d55df
Jean-Paul NGALULA <jeanpaulngalula07@gmail.com>
2024-08-12 23:48:21 +0200

    wip add new blog

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,32 @@ import { provideFileRouter } from '@analogjs/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import {
+  ContentRenderer,
+  MarkdownContentRendererService,
+  MarkedSetupService,
+  provideContent,
+  withHighlighter,
+  withMarkdownRenderer,
+} from '@analogjs/content';
+import { PrismHighlighter } from '@analogjs/content/prism-highlighter';
+
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-java';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    MarkedSetupService,
     provideFileRouter(),
     provideHttpClient(withFetch()),
     provideClientHydration(),
-    provideContent(withMarkdownRenderer()),
+    provideContent(
+      withMarkdownRenderer({
+        loadMermaid: () => import('mermaid'),
+      }),
+      withHighlighter({ useClass: PrismHighlighter })
+    ),
+    { provide: ContentRenderer, useClass: MarkdownContentRendererService },
   ],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     analog({
+      content: {
+        prismOptions: {
+          additionalLangs: ['java'],
+        },
+      },
       prerender: {
         routes: ['/', '/about', '/blog'],
       },
